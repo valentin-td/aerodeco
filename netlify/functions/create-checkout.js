c9982d6c31724fe7ac6ccf9a05c9fa51ddfc0baa
valentin-td <tardy.valentin04@gmail.com>
2026-04-12 16:07:04 +0200

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -3,8 +3,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 // 🛑 TON CATALOGUE SÉCURISÉ
 // C'est ici que tu définis tes prix. Stripe prend toujours les prix en CENTIMES. (ex: 89€ = 8900)
 const inventaire = {
-  "ADC-001": { nom: "Ariane Classic", prixCentimes: 8900 },
-  "ADC-002": { nom: "Saturn V Prestige", prixCentimes: 14900 },
+  "ADC-001": { nom: "Ariane Classic", prixCentimes: 100 },
+  "ADC-002": { nom: "Saturn V Prestige", prixCentimes: 100 },
   "ADC-003": { nom: "Falcon Gold Edition", prixCentimes: 22900 },
   "ADC-004": { nom: "Cosmos Bleu", prixCentimes: 7900 },
   "ADC-005": { nom: "Lourd Titan III", prixCentimes: 18900 },
